2ed409899f1aa386d28d1dbae337a3d416f47e6e
Jakub Kozioł <jakub.koziol12@gmail.com>
2018-06-16 03:59:06 +0200

    backup, partly done

--- file.py
+++ file.py
@@ -1,21 +1,89 @@
-import random
+import csv
 
 
-class File:
-    def __init__(self, filename):
-        self.open_file(filename)
+class TaskHandler:
+    def __init__(self, filename, team_velocity):
+        self.read_file(filename)
+        self.team_velocity = team_velocity
 
-    def open_file(self, filename):
+    def read_file(self, filename):
+        task_list = []
+
         try:
-            file = open(filename)
-            self.read_file(file)
+            with open(filename, 'r') as f:
+                # read data from file
+                reader = csv.DictReader(f)
 
-            # lines = file.read().splitlines()
-            # line = random.choice(lines)
-            # return line
+                for row in reader:
+                    ratio = self.count_ratio(row['story_points'], row['KSP'])
+
+                    row.update({'ratio': ratio})
+                    task_list.append(row)
+
+            self.choose_best_tasks(task_list)
         except IOError:
-            print("Can't open a file!")
+            print("Can't open the file! Did you type correct name?")
 
-    def read_file(self, file):
-        for line in file:
-            print(line)
+    @staticmethod
+    def count_ratio(story_points, KSP):
+        ratio = float(int(KSP) / int(story_points))
+        # setting precision to 3 decimal points
+        ratio = round(ratio, 3)
+        return ratio
+
+    def choose_best_tasks(self, task_list):
+
+        count_velocity = 0
+        best_tasks_ids = []
+
+        while count_velocity < self.team_velocity:
+            # before we start set best ratio to first task
+
+            best_task = task_list[0]
+
+            # find the best available task to take
+            for task in task_list:
+                if task['ratio'] > best_task['ratio']:
+                    best_task = task
+
+            # when we have best available task,check if not exceeding team velocity
+            count_velocity += best_task['ratio']
+            if count_velocity > self.team_velocity:
+
+                # deleting this task, because we can't use it anymore
+                task_list = self.delete_task(task_list, best_task['task_id'])
+                # if exceeding don't add
+                count_velocity -= best_task['ratio']
+                lasting_tasks = self.choose_best_with_condition(task_list, self.team_velocity - count_velocity)
+                task_list.append(lasting_tasks)
+            else:
+                best_tasks_ids.append(best_task['task_id'])
+                # delete this task, because w used it
+                task_list = self.delete_task(task_list, best_task['task_id'])
+
+    def choose_best_with_condition(self, task_list, points_left, other_ids=None):
+        if other_ids is None:
+            other_ids = []
+
+        best_ratio = 0
+        for task in task_list:
+            if task['story_points'] <= points_left and task['ratio'] > best_ratio:
+                best_ratio = task['ratio']
+                best_task = task
+
+        # if we used all points or didn't find any more matching tasks, we have nothing to do here
+        if points_left == 0 or best_ratio == 0:
+            return other_ids
+        else:
+            points_left -= best_task['story_points']
+            other_ids.append(best_task['task_id'])
+            task_list = self.delete_task(task_list, best_task['task_id'])
+
+            # call again recursively
+            self.choose_best_with_condition(task_list, points_left, other_ids)
+
+    def delete_task(self, task_list, task_id):
+        for task in task_list:
+            if task['task_id'] == task_id:
+                task_list.remove(task)
+                return task_list
--- sprint-planning-helper.py
+++ sprint-planning-helper.py
@@ -1,5 +1,5 @@
 import argparse
-from file import File
+from file import TaskHandler
 
 
 # here is where we start
@@ -7,12 +7,12 @@ def main():
     # parsing arguments passed when calling script
     parser = argparse.ArgumentParser(description="Choosing task file and max velocity")
     parser.add_argument('file', help="type: -file filename if you want to pass your file", type=str)
-    # parser.add_argument('velocity', help="1-easy, other-hard , default - easy", type=int)
+    parser.add_argument('velocity', help="1-easy, other-hard , default - easy", type=int)
 
     arg = parser.parse_args()
 
-    # pass a filename to file class constructor
-    File(arg.file)
+    # pass a filename to TaskHandler class constructor
+    TaskHandler(arg.file, arg.velocity)
 
 
 main()
